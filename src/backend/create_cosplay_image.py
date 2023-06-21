11964da6fe49e5f22b1d974093673c6cafc1a727
Atsuhisa Nakane <tontonkyu@gmail.com>
2023-06-21 16:25:59 +0900

    fix base functions

--- src/backend/create_cosplay_image.py
+++ src/backend/create_cosplay_image.py
@@ -13,48 +13,56 @@ from poisson_image_editing import poisson_edit
 
 def create_face_parts(image):
 
-  # use dlib
-  # reference: https://towardsdatascience.com/face-landmark-detection-using-python-1964cb620837
-  detector = dlib.get_frontal_face_detector()
-  predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
-  gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-  rects = detector(gray, 1)
+    # use dlib
+    # reference: https://towardsdatascience.com/face-landmark-detection-using-python-1964cb620837
+    detector = dlib.get_frontal_face_detector()
+    predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    rects = detector(gray, 1)
 
-  for rect in rects:
-      shape = predictor(gray, rect)
-      shape_np = np.zeros((68, 2), dtype="int")
-      for i in range(68):
-          shape_np[i] = (shape.part(i).x, shape.part(i).y)
-      shape = shape_np
-  return shape
+    for rect in rects:
+        shape = predictor(gray, rect)
+        shape_np = np.zeros((68, 2), dtype="int")
+        for i in range(68):
+            shape_np[i] = (shape.part(i).x, shape.part(i).y)
+        shape = shape_np
+    for i, (x, y) in enumerate(shape):
+      cv2.circle(image, (x, y), 1, (0, 0, 255), -1)
+    cv2.imshow('Landmark Detection', image)
+    cv2.waitKey()
+    return shape
 
-def create_mask_image(input_image):
-    face_points = create_cosplay_image(input_image)
-    print(face_points)
+def create_mask_image(human_image,anime_img):
+    human_face_points = create_face_parts(human_image)
+    anime_face_points = create_cosplay_image(anime_img)
+
+
     mask_image_path = ''
-    nose_point = (0, 0)
-    return mask_image_path, nose_point
+
+    return mask_image_path
 
 
-def calculate_offset(anime_nose_point, mask_nose_point):
-    return (0, 0)
+def calculate_main_parts(face_parts):
+    return 0
+
+def merge_images(human_img, anime_img):
+    mask_img_path = create_mask_image(human_img, anime_img)
+    mask_img = cv2.imread(mask_img_path)
+    result = poisson_edit(human_img, anime_img, mask_img, offset)
+    result = ''
+    return result
 
 
 def create_cosplay_image(human_img_path, anime_img_path):
     human_img = cv2.imread(human_img_path)
     anime_img = cv2.imread(anime_img_path)
-    mask_image_path, mask_nose_point = create_mask_image(human_img)
-    mask_img = cv2.imread(mask_image_path)
-    anime_nose_point = (0, 0)
-    offset = calculate_offset(anime_nose_point, mask_nose_point)
 
-    # poisson_blend_result = poisson_edit(human_img, anime_img, mask_img, offset)
-
+    merge_result = merge_images(human_img, anime_img)
     output_path = ''
     # cv2.imwrite(output_path, poisson_blend_result)
 
 
 if __name__ == '__main__':
-		human_img_path = 'resources/input/fuku.jpg'
+		human_img_path = '../images/初音ミク-1.png'
 		anime_img_path = ''
 		create_cosplay_image(human_img_path, anime_img_path)
