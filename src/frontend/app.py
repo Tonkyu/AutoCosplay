cd24dcb48ea5fbde6ee827b4d9310283d8885d32
tropical-summer <88573640+tropical-summer@users.noreply.github.com>
2023-06-27 16:31:17 +0900

    Update app.py

--- src/frontend/app.py
+++ src/frontend/app.py
@@ -1,4 +1,3 @@
-# app.py
 # streamlit run app.pyというコマンドをターミナルで実行して表示されるURLに以下を構築
 import streamlit as st
 import cv2
@@ -15,11 +14,8 @@ markdown = """
 """
 st.markdown(markdown)
 
-#backendにあるpythonファイル(create_cosplay_image.py,change_background.py)を動かすためにファイルパスを通す
 sys.path.append("../backend")
 
-
-#sys.pathを用いて動かしたいファイルをインポート
 #import create_cosplay_image
 import change_background
 
@@ -36,7 +32,6 @@ import change_background
 uploaded_completed_file = st.file_uploader("ファイルアップロードpngかjpg", type=["png", "jpg"])
 
 if uploaded_completed_file != None:
-    #st.file_uploader でアップロードされた PNG ファイルを PIL の Image オブジェクトに変換 (PILライブラリを使用したいため)
     img1 = Image.open(io.BytesIO(uploaded_completed_file.read()))
 
     markdown = """
@@ -47,16 +42,12 @@ if uploaded_completed_file != None:
 
     ###背景画像を表示して選ぶ機能###
 
-    # 画像を保存しているディレクトリのパス
     image_directory = "../images/backgrounds"
 
-    # 画像一覧を取得
     image_list = os.listdir(image_directory)
 
-    # 画像選択用のセレクトボックスを表示
     selected_image = st.selectbox("画像を選択", image_list)
 
-    # 選択した画像のファイルパスを取得
     image_path = os.path.join(image_directory, selected_image)
 
     # 画像を表示
@@ -64,7 +55,7 @@ if uploaded_completed_file != None:
         image = Image.open(image_path)
         st.image(image, caption="選択した画像", use_column_width=True)
 
-    #選んだ背景画像をimg2に渡す
+    
     img2=image_path
 
     ############################
@@ -75,7 +66,6 @@ if uploaded_completed_file != None:
         img1.save(temp_file.name)
         temp_file_path = temp_file.name
 
-    #img1はこのままだとPngImageFileオブジェクトであり、open()関数で直接扱えないので一時ファイルのファイルパスを渡す。
     img1=temp_file_path
 
     #背景交換実行
